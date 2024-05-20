d777cbf0ea17f8d8b5c60ccdc9e45ed83ebcd4b1
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-21 06:51:01 +0700

    js lab 68

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// write code here
+// var prompt = require('prompt-sync')();
+function Accumulator(startingValue) {
+    this.currentValue = startingValue;
+    this.read = function() {
+       return this.currentValue = parseFloat(prompt('Enter number: ', 0)) + startingValue
+    };
+    this.show = function() {
+       return this.currentValue
+    };
+}
+
+let acc = new Accumulator(1);
+acc.read()
+// console.log(acc.show())
+alert(acc.show())
+
